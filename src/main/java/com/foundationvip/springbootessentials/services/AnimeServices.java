96bce1119b0facd27b0937364c2091a8987e38ac
Carlos Aurelio <carlosync@gmail.com>
2022-07-17 17:42:51 -0400

    Implementing save of anime http POST

--- src/main/java/com/foundationvip/springbootessentials/services/AnimeServices.java
+++ src/main/java/com/foundationvip/springbootessentials/services/AnimeServices.java
@@ -5,12 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeServices {
 
-    private List<Anime> animes = List.of(new Anime(1L, "Naruto"), new Anime(2L, "Pokemon"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Naruto"), new Anime(2L, "Pokemon")));
+    }
+
     public List<Anime> listAll(){
         return animes;
     }
@@ -20,5 +27,11 @@ public class AnimeServices {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000));
+        animes.add(anime);
+        return anime;
     }
 }
